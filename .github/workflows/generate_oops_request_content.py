16dfdf5ef5a3b43a96436d582932dcfd0e427cd0
Matthias Jung <matthias.jung@iwm.fraunhofer.de>
2024-03-13 08:56:53 +0100

    add generate_oops_request_content tool

---
+++ .github/workflows/generate_oops_request_content.py
@@ -0,0 +1,42 @@
+"""Helper tool to generate a valid oops request xml file."""
+import xml
+import argparse
+#import rdflib
+
+def generate_request_content(input_path):
+    """
+    Generate the request content by reading the input file at the
+    specified path and creating an XML data structure from the content. 
+
+    Parameters:
+    input_path (str): The path to the input file.
+
+    Returns:
+    xml.etree.ElementTree.Element: The XML data structure representing the request content.
+    """
+    with open(input_path, 'r', encoding='utf-8') as f:
+        rdfcontent = f.read()
+
+    xmldata = xml.etree.ElementTree.fromstring("""
+        <?xml version="1.0" encoding="UTF-8"?>
+        <OOPSRequest>
+        <OntologyURI></OntologyURI>
+        <OntologyContent><![CDATA[ 
+         """ + rdfcontent + """
+         ]]></OntologyContent>
+        <Pitfalls></Pitfalls>
+        <OutputFormat>XML</OutputFormat>
+        </OOPSRequest>
+    """)
+    return xmldata
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-i', '--input', type=str, default='combined.rdf', help='Input file path'
+    )
+    parser.add_argument(
+        '-o', '--output', type=str, default='oops_request_content.xml', help='Output file path'
+    )
+    args = parser.parse_args()
+    generate_request_content(args.input).write(args.output)
